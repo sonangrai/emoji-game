83937daed22b941c81a431b734240c3d52b2e2d9
sonangrai <shonahangrae@gmail.com>
2025-03-01 20:12:42 +0545

    feat: implemented register api

--- apps/frontend/src/api/user.ts
+++ apps/frontend/src/api/user.ts
@@ -1,23 +1,27 @@
 import { API_URL } from "./config";
+import { UserPayload } from "../../../packages/shared/src";
 
 export const getUserByNickname = async (nickname: string) => {
   const res = fetch(`${API_URL}/user/${nickname}`, {
     method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
   });
 
   return res;
 };
 
-export const createUser = async (nickname: string) => {
-  const res = fetch(`${API_URL}/user/${nickname}`, {
+export const createUser = async (payload: UserPayload) => {
+  const res = await fetch(`${API_URL}/user`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
+    body: JSON.stringify(payload),
   });
 
-  return res;
+  if (!res.ok) {
+    const errorData = await res.json();
+    throw errorData;
+  }
+
+  return res.json();
 };
