71495774b1efa7672fdfd22be7464c1a07bdc0f1
sonangrai <shonahangrae@gmail.com>
2025-03-04 20:00:08 +0545

    fix: env fx

--- apps/frontend/src/api/config.ts
+++ apps/frontend/src/api/config.ts
@@ -1,6 +1,4 @@
 export const BASE_URL = `http://localhost:${process.env.PORT || 4000}`;
 
 export const API_URL =
-  process.env.NODE_ENV !== "production"
-    ? `${BASE_URL}/api`
-    : process.env.BASE_URL;
+  process.env.NODE_ENV !== "production" ? `${BASE_URL}/api` : "/api";
