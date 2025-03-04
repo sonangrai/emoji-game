6f493b0b120216ffbd857f0b65058f3851d89f7f
sonangrai <shonahangrae@gmail.com>
2025-03-04 19:38:19 +0545

    fix: fixed base URL for production

--- apps/frontend/src/api/config.ts
+++ apps/frontend/src/api/config.ts
@@ -1,2 +1,5 @@
-export const BASE_URL = `http://localhost:${process.env.PORT || 4000}`;
+export const BASE_URL =
+  process.env.NODE_ENV !== "production"
+    ? `http://localhost:${process.env.PORT || 4000}`
+    : process.env.BASE_URL;
 export const API_URL = `${BASE_URL}/api`;
