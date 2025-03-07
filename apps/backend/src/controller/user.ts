312ed2a036f43eb18bac21957da0f338ef7f8319
sonangrai <shonahangrae@gmail.com>
2025-03-07 18:19:53 +0545

    feat: register controller fix

--- apps/backend/src/controller/user.ts
+++ apps/backend/src/controller/user.ts
@@ -7,7 +7,7 @@ import ResponseObj from "./response";
  * Create a User
  */
 export const createUser = async (req: Request, res: Response) => {
-  //Checking the validation error
+  // Checking the validation error
   let errors = validationResult(req);
   if (!errors.isEmpty()) {
     let respObject = new ResponseObj(400, errors, "Validation error occurred");
@@ -16,12 +16,14 @@ export const createUser = async (req: Request, res: Response) => {
 
   try {
     let payload = {
-      Nickname: req.body.Nickname,
+      nickname: req.body.nickname,
+      email: req.body.email,
+      pin: req.body.pin,
     };
     let newUser = new User(payload);
 
     //Check if the nickname already exist
-    let found = await User.findOne({ Nickname: payload.Nickname });
+    let found = await User.findOne({ nickname: payload.nickname });
     if (found) {
       let respObject = new ResponseObj(400, {}, "Nickname already Taken");
       return res.status(400).send(respObject);
