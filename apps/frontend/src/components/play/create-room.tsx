2a1982a885bee819836e2007abf287407381e498
sonangrai <shonahangrae@gmail.com>
2025-03-24 16:52:45 +0545

    feat: create room api implemented

--- apps/frontend/src/components/play/create-room.tsx
+++ apps/frontend/src/components/play/create-room.tsx
@@ -3,18 +3,38 @@
 import { Dialog, DialogTrigger } from "@radix-ui/react-dialog";
 import { Button } from "../ui/button";
 import { DialogContent, DialogTitle } from "../ui/dialog";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
-import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { set, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "../ui/input";
+import { useMutation } from "@tanstack/react-query";
+import { createRoom } from "@/api/room";
+import { toast } from "sonner";
+import { getCookie } from "@/lib/cookie";
+import { queryClient } from "@/app/providers";
+import { useState } from "react";
 
 const roomFormSchema = z.object({
-  name: z.string().nonempty(),
-  password: z.string().optional(),
+  name: z.string().nonempty("Name is required"),
+  password: z.string().nonempty("Password is required"),
 });
 
 function CreateRoom() {
+  const [open, setOpen] = useState<boolean>(false);
+  const player = JSON.parse(getCookie("player"));
+  const createRoomMutation = useMutation({
+    mutationKey: ["create-room"],
+    mutationFn: createRoom,
+  });
+
   const form = useForm<z.infer<typeof roomFormSchema>>({
     resolver: zodResolver(roomFormSchema),
     defaultValues: {
@@ -24,11 +44,26 @@ function CreateRoom() {
   });
 
   function onSubmit(values: z.infer<typeof roomFormSchema>) {
-    console.log(values);
+    const payload = {
+      ...values,
+      _id: player._id,
+    };
+    return createRoomMutation.mutateAsync(payload, {
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: ["my-rooms"],
+        });
+        setOpen(false);
+        toast.success("Room created");
+      },
+      onError: () => {
+        toast.error("Failed to make room");
+      },
+    });
   }
 
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
         <Button>Create Room</Button>
       </DialogTrigger>
@@ -47,6 +82,7 @@ function CreateRoom() {
                     <FormControl>
                       <Input placeholder="Room Name" {...field} />
                     </FormControl>
+                    <FormMessage />
                   </FormItem>
                 )}
               />
@@ -64,11 +100,14 @@ function CreateRoom() {
                         {...field}
                       />
                     </FormControl>
+                    <FormMessage />
                   </FormItem>
                 )}
               />
 
-              <Button>Create</Button>
+              <Button type="submit" disabled={createRoomMutation.isPending}>
+                Create
+              </Button>
             </form>
           </Form>
         </div>
