052f45bdd6899ab8ee178352c5410e2d025ff2c8
sonangrai <shonahangrae@gmail.com>
2025-04-06 10:14:48 +0545

    feat: join room implemented

--- apps/frontend/src/components/play/index.tsx
+++ apps/frontend/src/components/play/index.tsx
@@ -1,12 +1,31 @@
 import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
 import CreateRoom from "./create-room";
+import JoinRoom from "./join-room";
 import MyRooms from "./my-rooms";
 
 function PlayPage() {
   return (
     <div className="">
       <div className="flex items-center justify-center gap-2 mb-4">
-        <Button variant="outline">Join Room</Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline">Join Room</Button>
+          </DialogTrigger>
+
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Join Room</DialogTitle>
+            </DialogHeader>
+            <JoinRoom />
+          </DialogContent>
+        </Dialog>
         <CreateRoom />
       </div>
       <MyRooms />
---
+++ apps/frontend/src/components/play/join-room.tsx
@@ -0,0 +1,96 @@
+"use client";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { useForm } from "react-hook-form";
+import { Button } from "../ui/button";
+import { useMutation } from "@tanstack/react-query";
+import { joinRoom } from "@/api/room";
+import { getCookie } from "@/lib/cookie";
+import { toast } from "sonner";
+
+const roomJoinSchema = z.object({
+  roomId: z.string().nonempty("Room ID is required"),
+  pin: z.string().nonempty("Pin is required"),
+});
+
+function JoinRoom() {
+  const player = getCookie("player");
+  const joinRoomMutation = useMutation({
+    mutationKey: ["join-room"],
+    mutationFn: joinRoom,
+  });
+
+  const form = useForm<z.infer<typeof roomJoinSchema>>({
+    defaultValues: {
+      roomId: "",
+      pin: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof roomJoinSchema>) {
+    const payload = {
+      rid: values.roomId,
+      userid: JSON.parse(player)._id as string,
+    };
+
+    return joinRoomMutation.mutateAsync(payload, {
+      onSuccess: () => {
+        toast.success("Joined");
+      },
+      onError: () => {
+        toast.error("Failed to join room");
+      },
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        className="flex flex-col gap-2 space-y-2"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <FormField
+          control={form.control}
+          name="roomId"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Room Id</FormLabel>
+              <FormControl>
+                <Input placeholder="Room Id" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="pin"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Room PIN</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="Room PIN" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" disabled={joinRoomMutation.isPending}>
+          Join
+        </Button>
+      </form>
+    </Form>
+  );
+}
+
+export default JoinRoom;
