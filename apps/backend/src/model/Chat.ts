326557341037a6d47b6f5572a249133aeaccb253
sonangrai <shonahangrae@gmail.com>
2025-04-12 19:58:44 +0545

    feat: added chat schema

---
+++ apps/backend/src/model/Chat.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const chatSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    room: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Room",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Room", chatSchema);
