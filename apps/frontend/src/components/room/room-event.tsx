85bd083dc6d4b4d06457917209df85e0ceeafee8
sonangrai <shonahangrae@gmail.com>
2025-04-12 13:08:44 +0545

    feat: room event component

---
+++ apps/frontend/src/components/room/room-event.tsx
@@ -0,0 +1,41 @@
+import useRoom from "@/hooks/useRoom";
+import { Alert, AlertTitle } from "../ui/alert";
+import { DoorOpen } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+function RoomEvent() {
+  const { roomEve } = useRoom();
+
+  return (
+    <div className="fixed bottom-10 left-12">
+      {roomEve.length > 0
+        ? roomEve.map((event, index) => (
+            <Alert
+              key={event.playerId + index}
+              className={cn(
+                "mb-2",
+                event.type === "JOIN" ? "border-green-500" : "border-red-500"
+              )}
+            >
+              <DoorOpen
+                className={
+                  event.type === "JOIN" ? "stroke-green-500" : "stroke-red-500"
+                }
+              />
+              <AlertTitle
+                className={cn(
+                  event.type === "JOIN" ? "text-green-500" : "text-red-500"
+                )}
+              >
+                {event.type === "JOIN"
+                  ? `${event.playerId} joined the room`
+                  : `${event.playerId} left the room`}
+              </AlertTitle>
+            </Alert>
+          ))
+        : null}
+    </div>
+  );
+}
+
+export default RoomEvent;
