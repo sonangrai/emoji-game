5886255067d370f6f724554ad361f47daac9c8f1
sonangrai <shonahangrae@gmail.com>
2025-04-13 16:42:43 +0545

    feat: updated type and removed system

--- apps/frontend/src/components/common/chat-bubble.tsx
+++ apps/frontend/src/components/common/chat-bubble.tsx
@@ -1,42 +1,38 @@
 import { cn } from "@/lib/utils";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Avatar, AvatarFallback } from "../ui/avatar";
+import { getCookie } from "@/lib/cookie";
 
 type ChatBubbleProps = {
-  message: string;
-  time: string;
-  isSender: boolean;
-  system?: boolean;
+  text: string;
+  createdAt: string;
   user: {
-    name: string;
-    image: string;
+    nickname: string;
   };
 };
 
 function ChatBubble(props: ChatBubbleProps) {
+  const nickName = JSON.parse(getCookie("player")).nickname;
+  const isSender = props.user.nickname === nickName;
+
   return (
     <div
       className={cn(
         "flex gap-2 items-center",
-        props.isSender ? "justify-end" : "justify-start"
+        isSender ? "justify-end" : "justify-start"
       )}
     >
-      {!props.isSender && (
-        <Avatar
-          className={props.system ? "border-blue-400 border" : ""}
-          title={props.user.name}
-        >
-          <AvatarImage src={props.user.image} />
-          <AvatarFallback>{props.user.name[0]}</AvatarFallback>
+      {!isSender && (
+        <Avatar title={props.user.nickname}>
+          <AvatarFallback>{props.user.nickname[0]}</AvatarFallback>
         </Avatar>
       )}
       <div
         className={cn(
           "inline-block py-2 px-4 rounded-lg",
-          props.isSender ? "bg-black text-white" : "bg-gray-200 text-black",
-          props.system && "bg-blue-500 text-white"
+          isSender ? "bg-black text-white" : "bg-gray-200 text-black"
         )}
       >
-        {props.message}
+        {props.text}
       </div>
     </div>
   );
