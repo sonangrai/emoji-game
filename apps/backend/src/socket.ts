96c3234a6971ac8cc5167221d3307d0453f3f6c9
sonangrai <shonahangrae@gmail.com>
2025-04-19 16:45:43 +0545

    feat: added player:left handle in socket

--- apps/backend/src/socket.ts
+++ apps/backend/src/socket.ts
@@ -1,6 +1,10 @@
 // socket.ts
 import { Server as HttpServer } from "http";
 import { Server as SocketIOServer } from "socket.io";
+import { leaveRoom } from "./controller/room";
+import Room from "./model/Room";
+import { LEAVE_ROOM } from "./events/room";
+import User from "./model/User";
 
 let io: SocketIOServer;
 
@@ -14,6 +18,28 @@ export function initSocket(server: HttpServer): SocketIOServer {
 
   io.on("connection", (socket) => {
     console.log("Client connected:", socket.id);
+
+    socket.on("PLAYER:LEFT", async (d) => {
+      const userData = await User.findById(d.pid);
+
+      await Room.findOneAndUpdate(
+        {
+          _id: d.rid,
+          "players.user": d.pid,
+        },
+        {
+          $set: {
+            "players.$.online": false,
+          },
+        },
+        { new: true }
+      );
+
+      io.emit(LEAVE_ROOM, {
+        userId: d.pid,
+        nickname: userData?.nickname,
+      });
+    });
   });
 
   return io;
